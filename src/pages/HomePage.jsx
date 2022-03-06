d351d2211166e8ac80e8a104f839c4cebd052905
Evhan Joseph <evhanjhpro@outlook.com>
2022-03-06 20:11:12 +0100

    Init Project

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,16 +6,29 @@ export default function HomePage() {
         <>
             <div className="p-5 mb-4 bg-light rounded-3">
                 <div className="container-fluid py-5">
-                    <h1 className="display-5 fw-bold">Application SPA</h1>
+                    <h1 className="display-5 fw-bold">Application Meteo</h1>
                     <p className="col-md-8 fs-4">
-                        Exemple d'utilisation de React en multipages avec la librairie React
-                        Router.
+                        Ce projet consiste à visualiser la météo des villes que l'on souhaite
                     </p>
 
                     <p>
-                        Cette application va nous permettre de saisir des EAN pour retrouver
-                        des produits alimentaires et les enregistrer dans notre base de
-                        données
+                        Toutes les requêtes sont utilisés, mise à part GET si je me trompe au niveau du serveur.
+                        <br />
+                        <br />
+                        L'application comporte :
+                        <br />
+                        - Un système de connexion/inscription/deconnexion
+                        <br />
+                        - Une page de modification du compte ou l'on peut modifier le mot de passe
+                        <br />
+                        - Une page MyMeteo une fois connecté ou l'on peut ajouter des météos que l'on souhaite qui seront sauvegardés
+                        <br />
+                        - Une page Meteo avec les météos à visualiser
+                        <br />
+                        - Un système de géolocalisation
+                        <br />
+                        <br />
+                        On utilise donc les appels post/put/delete
                     </p>
                     <Button variant="primary" size="lg" as={Link} to="/meteo">
                         Trouver ma météo
